166e3d5b78f4dd5295a9bfb0847b78b8578a35d2
JameikBadia-Nevels <jameiknevels@gmail.com>
2022-03-08 15:27:30 -0500

    trying to get it to work

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use((req,res,next)=>{
     next()
 })
 //keep this near the top
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({extended:true}))
 
 
 
@@ -101,4 +101,4 @@ mongoose.connection.once('open', ()=> {
 
 app.listen(PORT, () => {
     console.log('We in the building', PORT)
-  })
+})
